e131a828de365b1855ecc456b87689b7df86f0a5
Walison Theodoro <theodoro.walison@gmail.com>
2022-08-02 09:47:05 -0300

    fix: disable cors origins

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ const server = app.listen(process.env.PORT, () => {
 })
 
 const io = socket(server, {
-  cors: {
-    origin: 'http://localhost:3000',
-    credentials: true,
+  allowRequest: (req, callback) => {
+    const noOriginHeader = req.headers.origin === undefined
+    callback(null, noOriginHeader)
   },
 })
 
